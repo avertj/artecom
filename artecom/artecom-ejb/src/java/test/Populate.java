9ca9fe213eebc103938a59ec17f1305c975287a4
Don <donatien@Ubuntu-Don>
2014-09-10 11:21:48 +0200

    mm

--- artecom/artecom-ejb/src/java/test/Populate.java
+++ artecom/artecom-ejb/src/java/test/Populate.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package test;
 
 /**
@@ -15,5 +14,5 @@ public class Populate {
     public Populate() {
         // TODO code application logic here
     }
-    
+
 }
